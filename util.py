79a94c115ec2403cf1cf8a2eea2cfbbbbc5367a1
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-03-31 11:11:46 +0100

    util: use block range instead of indices

--- util.py
+++ util.py
@@ -38,29 +38,29 @@ def plot_data(block_heights, mempool_estimates, blockpolicy_estimates, low_perce
     ax.plot(block_heights, blockpolicy_estimates, linewidth=2, color='yellow', label='Block Policy Estimates')
     ax.plot(block_heights, mempool_estimates, linewidth=2, color='blue', label='Mempool Estimates')
 
-    plt.title("Mempool fee estimate vs estimatesmartfee vs Actual block median fee", loc="left", fontsize=12, fontstyle='italic')
-    plt.suptitle("All with confirmation target 1", y=0.92, fontsize=10, fontweight='bold')
+    plt.title("Mempool fee estimate vs estimatesmartfee against the block 5th percentile to 75th percentile fee rate", loc="left", fontsize=12, fontstyle='italic')
+    plt.suptitle("With confirmation target 1", y=0.92, fontsize=10, fontweight='bold')
 
     plt.xlabel("Block Height", fontsize=10, fontweight='bold')
     plt.ylabel("Fee Estimates", fontsize=10, fontweight='bold')
     plt.legend()
     plt.show()
 
-def plot_estimates(start=0, end=1500, data=None):
+def plot_estimates(start, end, data):
     """
     Plots fee estimates.
 
     Parameters:
-    start (int): Starting index.
-    end (int): Ending index.
+    start (int): Starting block.
+    end (int): Ending block.
     data (list): List of dictionaries containing mempool data.
     """
-    MAX_THRESHOLD = 2000
+    MAX_THRESHOLD = 200
     if (end - start) > MAX_THRESHOLD:
-        print(f"Data point exceeded maximum threshold of {MAX_THRESHOLD}")
+        print(f"Blocks exceeded maximum threshold of {MAX_THRESHOLD} Blocks")
         return
     
-    if start < 0 or end >= len(data):
+    if start < 0 or end >= int(float(data[len(data) -1]["block_height"])):
         print("Invalid range")
         return
 
@@ -70,13 +70,17 @@ def plot_estimates(start=0, end=1500, data=None):
     low_percentile = [] 
     high_percentile = []
 
-    while(start < end):
-        block_heights.append(int(float(data[start]["block_height"])))
-        mempool_estimates.append(int(float(data[start]["mempool_fee_rate_estimate"]) / 1000))
-        blockpolicy_estimates.append(int(float(data[start]["block_fee_rate_estimate"]) / 1000))
-        low_percentile.append(int(float(data[start][".05"])))
-        high_percentile.append(int(float(data[start][".75"])))
-        start +=1
+    i = 0
+    while(i < len(data) and end >= int(float(data[i]["block_height"]))):
+        blk_height = int(float(data[i]["block_height"]))
+        if start >= blk_height:
+            block_heights.append(int(float(data[i]["block_height"])))
+            mempool_estimates.append(int(float(data[i]["mempool_fee_rate_estimate"]) / 1000))
+            blockpolicy_estimates.append(int(float(data[i]["block_fee_rate_estimate"]) / 1000))
+            low_percentile.append(int(float(data[i][".05"])))
+            high_percentile.append(int(float(data[i][".75"])))
+            start+=1
+        i+=1
 
     plot_data(block_heights, mempool_estimates, blockpolicy_estimates, low_percentile, high_percentile)
 
