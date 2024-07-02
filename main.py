2051e0de00d4f6adfcdea974b7e5b664d3b2f9b9
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-07-02 11:32:43 +0100

    forecast-analysis

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from util import get_summary, plot_estimates, read_json_file
+from util import get_summary, plot_mempool_estimates, plot_forecaster_estimates, read_json_file
 
 file_dir = "forcast_output.json"
-MempoolLast10Min = "Mempool Last 10 min Forecast"
-Mempool = "Mempool Forecast"
-LastBlock = "Last Block Forecast"
-Last6Blocks = "Block Forecast"
-Conservative = "conservative_fee_rate"
-Economic = "economic_fee_rate"
+data = read_json_file(file_dir, BitcoindThreshold=False, mode="conservative_fee_rate")
 
-data = read_json_file(file_dir, BitcoindThreshold=False, mode=Conservative)
+# Compare two estimates estimates
+plot_mempool_estimates(848920, 849120, data, logscale_yaxis=True)
 
-plot_estimates(848920, 849120, data, forecaster=Last6Blocks, logscale_yaxis=True)
+# Plot forecaster estimates
+plot_forecaster_estimates(848920, 849120, data, forecaster="Block Forecast", logscale_yaxis=True)
 
-get_summary(data, forecaster=Last6Blocks)
+# Get summary
+get_summary(data, forecaster="Block Forecast")
--- util.py
+++ util.py
@@ -1,9 +1,28 @@
 import matplotlib.pyplot as plt
 import json
-
 from datetime import datetime
 
-def read_json_file(file_path, BitcoindThreshold, mode):
+MempoolLast10Min = "Mempool Last 10 min Forecast"
+Mempool = "Mempool Forecast"
+LastBlock = "Last Block Forecast"
+Last6Blocks = "Block Forecast"
+Conservative = "conservative_fee_rate"
+Economic = "economic_fee_rate"
+
+def sats_per_kb_to_sats_per_byte(sats_per_kb):
+    """
+    Converts satoshis per kilobyte to satoshis per byte.
+
+    Parameters:
+    sats_per_kb (float): Fee rate in satoshis per kilobyte.
+
+    Returns:
+    int: Fee rate in satoshis per byte.
+    """
+    return int(float(sats_per_kb) / 1000)
+
+
+def read_json_file(file_path, BitcoindThreshold=False, mode=Conservative):
     """
     Reads estimates data from a JSON file.
 
@@ -21,52 +40,46 @@ def read_json_file(file_path, BitcoindThreshold, mode):
                     "timestamp": entry["timestamp"],
                     "block_height": int(float(entry["block_height"])),
                     "forecaster": entry["forecaster"],
-                    "low": int(float(entry["low"]) / 1000),
-                    "high": int(float(entry["high"]) / 1000),
-                    "conservative_fee_rate": int(float(entry["conservative_fee_rate"]) / 1000),
-                    "economic_fee_rate": int(float(entry["economic_fee_rate"]) / 1000),
-                    "p_5": int(float(entry["p_5"]) / 1000),
-                    "p_50": int(float(entry["p_50"]) / 1000)
+                    "low": sats_per_kb_to_sats_per_byte(entry["low"]),
+                    "high": sats_per_kb_to_sats_per_byte(entry["high"]),
+                    "conservative_fee_rate": sats_per_kb_to_sats_per_byte(entry["conservative_fee_rate"]),
+                    "economic_fee_rate": sats_per_kb_to_sats_per_byte(entry["economic_fee_rate"]),
+                    "p_5": sats_per_kb_to_sats_per_byte(entry["p_5"]),
+                    "p_50": sats_per_kb_to_sats_per_byte(entry["p_50"])
                 }
                 for entry in data if 'conservative_fee_rate' in entry and 'p_5' in entry
             ]
             if BitcoindThreshold:
-                for data in new_data:
-                    if data["high"] > data[mode]:
-                        data["high"] = data[mode]
-                    if data["low"] > data[mode]:
-                        data["low"] = data[mode]
-                    
+                for d in new_data:
+                    for key in ["high", "low"]:
+                        if d[key] > d[mode]:
+                            d[key] = d[mode]
             return new_data
     except Exception as e:
         print(f"Failed to load estimates data from {file_path}: {e}")
         return []
 
-def plot_data(forecaster, block_heights, low_priority_estimates, high_priority_estimates, 
-              conservative_estimates, economic_estimates, low_percentile, high_percentile, logscale_yaxis):
+def plot_data(block_heights, estimates_dict, low_percentile, high_percentile, logscale_yaxis, labels):
     """
     Plots estimates data.
 
     Parameters:
     block_heights (list): List of block heights.
-    low_priority_estimates (list): List of low priority estimates.
-    high_priority_estimates (list): List of high priority estimates.
-    conservative_estimates (list): List of conservative fee estimates.
-    economic_estimates (list): List of economic fee estimates.
+    estimates_dict (dict): Dictionary containing lists of different estimates.
     low_percentile (list): List of low percentile values.
     high_percentile (list): List of high percentile values.
     logscale_yaxis (bool): Flag to set y-axis to logarithmic scale.
+    labels (dict): Dictionary containing labels for the plot.
     """
     plt.style.use('ggplot')
     fig, ax = plt.subplots(figsize=(15, 15))
 
     ax.fill_between(block_heights, low_percentile, high_percentile, alpha=.5, linewidth=0, color='grey', label='5th to 50th percentile')
-    ax.plot(block_heights, high_priority_estimates, linewidth=2, color='yellow', label=f'{forecaster} high')
-    ax.plot(block_heights, low_priority_estimates, linewidth=2, color='blue', label=f'{forecaster} Low')
-    ax.plot(block_heights, economic_estimates, linewidth=2, color='green', label='Bitcoind Economic')
-    ax.plot(block_heights, conservative_estimates, linewidth=2, color='red', label='Bitcoind Conservative')
+    
+    for key, values in estimates_dict.items():
+        ax.plot(block_heights, values, linewidth=2, label=labels[key])
 
-    plt.title("Estimators against the block 5th percentile to 75th percentile fee rate", loc="left", fontsize=12, fontstyle='italic')
+    plt.title("Estimators against the block 5th percentile to 50th percentile fee rate", loc="left", fontsize=12, fontstyle='italic')
     plt.suptitle("With confirmation target 1", y=0.92, fontsize=10, fontweight='bold')
 
     plt.xlabel("Block Height", fontsize=10, fontweight='bold')
@@ -76,9 +89,76 @@ def plot_data(forecaster, block_heights, low_priority_estimates, high_priority_e
         ax.set_yscale('log')
     plt.show()
 
-def plot_estimates(start, end, data, forecaster, logscale_yaxis=False):
+def validate_data_range(start, end, data):
     """
-    Plots fee estimates.
+    Validates the data range for plotting.
+
+    Parameters:
+    start (int): Starting block.
+    end (int): Ending block.
+    data (list): List of dictionaries containing estimates data.
+
+    Returns:
+    bool: True if the range is valid, False otherwise.
+    """
+    if not data:
+        print("Fee estimates data not provided")
+        return False
+
+    MAX_THRESHOLD = 200
+    if (end - start) > MAX_THRESHOLD:
+        print(f"Blocks exceeded maximum threshold of {MAX_THRESHOLD} Blocks")
+        return False
+
+    if start < 0 or end >= data[-1]["block_height"]:
+        print("Invalid range")
+        return False
+
+    return True
+
+def plot_mempool_estimates(start, end, data, logscale_yaxis=False):
+    """
+    Plots mempool fee estimates.
+
+    Parameters:
+    start (int): Starting block.
+    end (int): Ending block.
+    data (list): List of dictionaries containing estimates data.
+    logscale_yaxis (bool): Flag to set y-axis to logarithmic scale.
+    """
+    if not validate_data_range(start, end, data):
+        return
+
+    filtered_data = [entry for entry in data if start <= entry["block_height"] <= end and entry["forecaster"] in {Mempool, MempoolLast10Min}]
+    
+    if not filtered_data:
+        print("No data found for the given range and forecaster criteria")
+        return
+
+    block_heights = [entry["block_height"] for entry in filtered_data if entry["forecaster"] == MempoolLast10Min]
+    mempool_high_estimates = [entry["high"] for entry in filtered_data if entry["forecaster"] == Mempool]
+    mempool_last_10_low_estimates = [entry["low"] for entry in filtered_data if entry["forecaster"] == MempoolLast10Min]
+    low_percentile = [entry["p_5"] for entry in filtered_data if entry["forecaster"] == MempoolLast10Min]
+    high_percentile = [entry["p_50"] for entry in filtered_data if entry["forecaster"] == MempoolLast10Min]
+
+    if len(mempool_high_estimates) != len(mempool_last_10_low_estimates):
+        print("Mismatch in the lengths of mempool high and last 10 min low estimates")
+        return
+
+    estimates_dict = {
+        "mempool_high": mempool_high_estimates,
+        "mempool_last_10_low": mempool_last_10_low_estimates
+    }
+    labels = {
+        "mempool_high": "Mempool High Estimates",
+        "mempool_last_10_low": "Mempool Last 10 Min Low Estimates"
+    }
+
+    plot_data(block_heights, estimates_dict, low_percentile, high_percentile, logscale_yaxis, labels)
+
+def plot_forecaster_estimates(start, end, data, forecaster, logscale_yaxis=False):
+    """
+    Plots forecaster fee estimates.
 
     Parameters:
     start (int): Starting block.
@@ -87,23 +167,15 @@ def plot_estimates(start, end, data, forecaster, logscale_yaxis=False):
     forecaster (str): The name of the forecaster.
     logscale_yaxis (bool): Flag to set y-axis to logarithmic scale.
     """
-    if not data:
-        print("Fee estimates data not provided")
+    if not validate_data_range(start, end, data):
         return
 
-    MAX_THRESHOLD = 200
-    if (end - start) > MAX_THRESHOLD:
-        print(f"Blocks exceeded maximum threshold of {MAX_THRESHOLD} Blocks")
+    filtered_data = [entry for entry in data if start <= entry["block_height"] <= end and entry["forecaster"] == forecaster]
+    
+    if not filtered_data:
+        print(f"No data found for forecaster {forecaster}")
         return
 
-    if start < 0 or end >= data[-1]["block_height"]:
-        print("Invalid range")
-        return
-
-    filtered_data = [
-        entry for entry in data
-        if start <= entry["block_height"] <= end and entry["forecaster"] == forecaster
-    ]
     block_heights = [entry["block_height"] for entry in filtered_data]
     low_priority_estimates = [entry["low"] for entry in filtered_data]
     high_priority_estimates = [entry["high"] for entry in filtered_data]
@@ -112,9 +184,21 @@ def plot_estimates(start, end, data, forecaster, logscale_yaxis=False):
     low_percentile = [entry["p_5"] for entry in filtered_data]
     high_percentile = [entry["p_50"] for entry in filtered_data]
 
-    plot_data(forecaster, block_heights, low_priority_estimates, high_priority_estimates,
-              conservative_estimates, economic_estimates, low_percentile, high_percentile, logscale_yaxis)
+    estimates_dict = {
+        "high_priority": high_priority_estimates,
+        "low_priority": low_priority_estimates,
+        "conservative": conservative_estimates,
+        "economic": economic_estimates
+    }
 
+    labels = {
+        "high_priority": "High Priority Estimates",
+        "low_priority": "Low Priority Estimates",
+        "conservative": "Bitcoind Conservative",
+        "economic": "Bitcoind Economic"
+    }
+
+    plot_data(block_heights, estimates_dict, low_percentile, high_percentile, logscale_yaxis, labels)
 
 def calculate_percentages(data, key):
     total = len(data)
@@ -123,11 +207,11 @@ def calculate_percentages(data, key):
     underpaid = sum(1 for row in data if row[key] < row["p_5"])
     overpaid = sum(1 for row in data if row[key] > row["p_50"])
     within_range = total - underpaid - overpaid
-    arr = []
-    arr.append((overpaid, (overpaid / total * 100)))
-    arr.append((underpaid, (underpaid / total * 100)))
-    arr.append((within_range, (within_range / total * 100)))
-    return arr
+    return [
+        (overpaid, (overpaid / total * 100)),
+        (underpaid, (underpaid / total * 100)),
+        (within_range, (within_range / total * 100))
+    ]
 
 def print_summary(data, key):
     arr = calculate_percentages(data, key)
@@ -135,18 +219,17 @@ def print_summary(data, key):
         print(f"{count} estimates {category} {percentage:.2f}% of the total estimates ")
 
 def get_summary(data, forecaster):
-    total = len(data)
-    if total == 0:
+    filtered_data = [entry for entry in data if entry["forecaster"] == forecaster]
+    
+    if not filtered_data:
+        print(f"No data found for forecaster {forecaster}")
         return
 
-    filtered_data = [
-        entry for entry in data
-        if entry["forecaster"] == forecaster
-    ]
+    total = len(filtered_data)
     start_time = datetime.fromisoformat(filtered_data[0]['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
     end_time = datetime.fromisoformat(filtered_data[-1]['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
-    start_block = int(filtered_data[0]['block_height']) - 1
-    end_block = int(filtered_data[-1]['block_height']) - 1
+    start_block = filtered_data[0]['block_height'] - 1
+    end_block = filtered_data[-1]['block_height'] - 1
 
     print(f"Total of {total} estimates were made from {start_time} to {end_time} from Block {start_block} to Block {end_block}")
     print("---------------------------------------------------------")
@@ -154,16 +237,13 @@ def get_summary(data, forecaster):
     print_summary(filtered_data, "low")
 
     print("---------------------------------------------------------")
-    print("High priority estimate")
+    print("High priority estimates")
     print_summary(filtered_data, "high")
 
     print("---------------------------------------------------------")
-    print("Bitcoind conservative estimate")
+    print("Bitcoind conservative estimates")
     print_summary(filtered_data, "conservative_fee_rate")
 
     print("---------------------------------------------------------")
-    print("Bitcoind economic estimate")
+    print("Bitcoind economic estimates")
     print_summary(filtered_data, "economic_fee_rate")
-
-    
-        
